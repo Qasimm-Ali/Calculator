4016180432ffa08ecfc8efc245216a549873e27d
Qasimm-Ali <qqali776655@gmail.com>
2024-06-06 13:21:35 +0500

    Adding Functionality

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -3,12 +3,24 @@ export default function Buttons(){
     const [first, setfirst] = useState('')
     const btnz = [
         "1","2","3","4","5","6","7","8","9","0","+","-","C","/","%","*","=" ]
+        const handleClick = (e) =>{
+            if(e === "C" ){
+                setfirst("")
+            }else if(e === "="){
+                const res = eval(first)
+                setfirst(res)
+            }else{
+                const result = first + e
+                setfirst(result)
+            }
+        }
+        
     return(
         <div>
             <p><input type="text" value={first}  /></p>
             {
                 btnz.map((item, ind) =>(
-                    <button key={ind} onClick={handleClick}> {item} </button>
+                    <button key={ind} onClick={() =>handleClick(item)}> {item} </button>
                 ))
             }
         </div>
